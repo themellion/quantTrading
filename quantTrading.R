1a459dfc3e8e249dd6b95955de30b76949d55dcc
Stan F <40437425+themellion@users.noreply.github.com>
2018-12-07 17:56:09 +0000

    Add files via upload

--- quantTrading.R
+++ quantTrading.R
@@ -6,21 +6,21 @@ last.date <- Sys.Date()
 freq.data <- 'daily'
 
 # set tickers
-tickers <- c('FB','MMM','PETR4.SA','abcdef') # Facebook, Petrobras, 3M
+# tickers <- c('FB','MMM','PETR4.SA','abcdef') # Facebook, Petrobras, 3M
+# 
+# l.out <- BatchGetSymbols(tickers = tickers, 
+#                          first.date = first.date,
+#                          last.date = last.date, 
+#                          freq.data = freq.data,
+#                          cache.folder = file.path(tempdir(), 
+#   'BGS_Cache') ) # cache in tempdir()
+# 
+# print(l.out)
 
-l.out <- BatchGetSymbols(tickers = tickers, 
-                         first.date = first.date,
-                         last.date = last.date, 
-                         freq.data = freq.data,
-                         cache.folder = file.path(tempdir(), 
-  'BGS_Cache') ) # cache in tempdir()
-
-print(l.out)
-
-p <- ggplot(l.out$df.tickers, aes(x = ref.date, y = price.close))
-p <- p + geom_line()
-p <- p + facet_wrap(~ticker, scales = 'free_y') 
-print(p)
+# p <- ggplot(l.out$df.tickers, aes(x = ref.date, y = price.close))
+# p <- p + geom_line()
+# p <- p + facet_wrap(~ticker, scales = 'free_y') 
+# print(p)
 
 # download data for MARUTI ~ i.e. MARUTI.NS
 myTicker <- 'MARUTI.NS'
@@ -33,3 +33,7 @@ l.out <- BatchGetSymbols(tickers = myTicker,
 
 # keep specific columns
 stockDF <- l.out$df.tickers[, c('ref.date', 'price.open', 'price.high', 'price.low', 'price.close', 'volume')]
+
+p <- ggplot(stockDF, aes(x = ref.date, y = price.close))
+p <- p + geom_line() + xlab('Date') + ylab('Closing Price') + ggtitle('MARUTI (MARUTI.NS)')
+print(p)
